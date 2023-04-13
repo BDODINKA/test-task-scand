d22bc0415d67b193c4bf3cbbd50500e54aeb8fb5
DMITRY <bdodinka1992@gmail.com>
2023-04-13 21:22:53 +0300

    fix:IAuthor interface and Input rest props

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -4,7 +4,7 @@ import style from '@/styles/Input/input.module.scss'
 
 export const Input: FC<
   DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
-> = (className, ...rest) => {
+> = ({ className, ...rest }) => {
   const finalClass = className ? `${style.input} ${className}` : style.input
 
   return <input type="email" className={finalClass} {...rest} />
--- src/interface/IAuthor.ts
+++ src/interface/IAuthor.ts
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import { StaticImageData } from 'next/image'
+
 export interface IAuthor {
-  ava: string
+  ava: string | StaticImageData
   children: React.ReactNode
   className?: string
 }
